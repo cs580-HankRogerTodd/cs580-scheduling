9c1ace7da9cea9a1468aa84d5085fbbd73b19de6
toddnguyen47 <todd.nguyen47@gmail.com>
2018-01-24 17:47:44 -0800

    Can change calendar months!

--- src/main/java/cs580/ScheduleCalendar2.java
+++ src/main/java/cs580/ScheduleCalendar2.java
@@ -6,20 +6,31 @@ import java.time.YearMonth;
 
 import javax.swing.JFrame;
 import javax.swing.JTable;
+import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableModel;
+
+import customComponents.ResizableButton;
+
 import javax.swing.ListSelectionModel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ScheduleCalendar2 {
 
 	private JFrame frame;
 	private JTable table;
 	
+	private int currentMonth = LocalDate.now().getMonthValue();
+	private int currentYear = LocalDate.now().getYear();
+	
 	private static String DAYS_OF_WEEK[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
 	private static String MONTHS[] = {"January", "February", "March", "April", "May", "June", "July",
 			"August", "September", "October", "November", "December"};
+	
+	private JLabel lblNewLabel;
 
 	// Launch the application.	 
 	public static void main(String[] args) {
@@ -48,6 +59,7 @@ public class ScheduleCalendar2 {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.setTitle("CS580 Scheduling - Group 7");
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
@@ -56,11 +68,8 @@ public class ScheduleCalendar2 {
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		table.setRowSelectionAllowed(false);
 		
-		DefaultTableModel model = new DefaultTableModel(generateDaysInMonth(),
-				new String[] {
-						"New column", "New column", "New column", "New column",
-						"New column", "New column", "New column"
-					}) {
+		DefaultTableModel model = new DefaultTableModel(generateDaysInMonth(currentYear, currentMonth),
+				DAYS_OF_WEEK) {
 						private static final long serialVersionUID = 1L;
 
 						public boolean isCellEditable(int row, int column) {
@@ -73,26 +82,62 @@ public class ScheduleCalendar2 {
 		table.setBounds(65, 54, 324, 168);
 		frame.getContentPane().add(table);
 		
-		JLabel lblNewLabel = new JLabel();
-		int curMonth = LocalDate.now().getMonthValue();
-		lblNewLabel.setText(MONTHS[curMonth - 1]);
-		lblNewLabel.setBounds(196, 24, 46, 14);
+		lblNewLabel = new JLabel();
+		lblNewLabel.setBounds(199, 20, 65, 23);
 		frame.getContentPane().add(lblNewLabel);
+		lblNewLabel.setText(MONTHS[currentMonth - 1]);
 		
-		JButton button = new JButton(">");
+		ResizableButton button = new ResizableButton(">");
 		button.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		button.setBounds(247, 20, 46, 23);
+		button.setBounds(274, 20, 46, 23);
 		frame.getContentPane().add(button);
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				DefaultTableModel tempModel = (DefaultTableModel) table.getModel();
+				tempModel.setRowCount(0);
+				
+				currentMonth++;
+				if (currentMonth > MONTHS.length) {
+					currentMonth = 1;
+				}
+				
+				lblNewLabel.setText(MONTHS[currentMonth - 1]);
+				Object[][] tempData = generateDaysInMonth(2018, currentMonth);
+				table.setModel(new DefaultTableModel(tempData, DAYS_OF_WEEK));
+				((AbstractTableModel)table.getModel()).fireTableDataChanged();
+			}
+		});
+		
+		JButton button_1 = new JButton("<");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				currentMonth--;
+				if (currentMonth <= 0) {
+					currentMonth = MONTHS.length;
+				}
+				
+				lblNewLabel.setText(MONTHS[currentMonth - 1]);
+				Object[][] tempData = generateDaysInMonth(2018, currentMonth);
+				table.setModel(new DefaultTableModel(tempData, DAYS_OF_WEEK));
+				((AbstractTableModel)table.getModel()).fireTableDataChanged();
+			}
+		});
+		button_1.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		button_1.setBounds(129, 20, 46, 23);
+		frame.getContentPane().add(button_1);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.setBounds(244, 233, 89, 23);
+		frame.getContentPane().add(btnCancel);
 	}
 	
-	private Object[][] generateDaysInMonth() {
-		int rowCounter = 5; // 5 rows to display month
+	private Object[][] generateDaysInMonth(int year, int month) {
+		int rowCounter = 6; // 6 rows to display month
 		int colCounter = 7;
 				
-		// Adding January
-		int lengthOfMonth = YearMonth.of(2018, 1).lengthOfMonth();
-		LocalDate temp = LocalDate.of(2018, 1, 1);
-		int dayOfFirstDayOfMonth = temp.getDayOfWeek().getValue() % 7;
+		int lengthOfMonth = YearMonth.of(year, month).lengthOfMonth();
+		LocalDate tempLocalDate = LocalDate.of(year, month, 1);
+		int dayOfFirstDayOfMonth = tempLocalDate.getDayOfWeek().getValue() % 7;
 		
 		// If the month is February and the first day is Monday
 		/*if (dayOfFirstDayOfMonth == 0)
@@ -107,7 +152,7 @@ public class ScheduleCalendar2 {
 		for (int i = 1; i <= lengthOfMonth; i++) {
 			if (isFirstWeek)
 				curCol = dayOfFirstDayOfMonth;
-			
+					
 			days[curRow][curCol] = i;
 			curCol++;
 			
@@ -122,5 +167,14 @@ public class ScheduleCalendar2 {
 		}
 		
 		return days;
+	}
+	
+	//*****************************
+	// Getter and setters
+	//*****************************
+	public int getCurrentMonth() {return currentMonth;}
+	
+	public void print(Object o) {
+		System.out.println(o);
 	}
 }

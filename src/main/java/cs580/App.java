8a0617df4984f3ce5b9a8e0a271b8cfc97c6460b
Roger Halftermeyer <rhalf001@gmail.com>
2018-01-30 21:21:00 -0800

    Added ways to read the subDocuments and new meeting time
    added more commepts on the java app to help store and update times for the employees. Also added ways to read the stored information.

--- src/main/java/cs580/App.java
+++ src/main/java/cs580/App.java
@@ -7,6 +7,7 @@ package cs580;
 import java.time.format.DateTimeFormatter;
 
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
@@ -23,11 +24,11 @@ public class App {
 		LoginPage login = new LoginPage();
 		
 		//this connects to the database
-//		String uri = "mongodb://rhalf001:admin@580scheduledb-shard-00-00-w3srb.mongodb.net:27017,580scheduledb-shard-00-01-w3srb.mongodb.net:27017,580scheduledb-shard-00-02-w3srb.mongodb.net:27017/test?ssl=true&replicaSet=580scheduleDB-shard-0&authSource=admin";
-//		MongoClientURI clientUri = new MongoClientURI(uri);
-//		MongoClient mongoClient = new MongoClient(clientUri);
-//		MongoDatabase mongoDatabase = mongoClient.getDatabase("580Schedule");
-//		MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("Users");
+		String uri = "mongodb://rhalf001:admin@580scheduledb-shard-00-00-w3srb.mongodb.net:27017,580scheduledb-shard-00-01-w3srb.mongodb.net:27017,580scheduledb-shard-00-02-w3srb.mongodb.net:27017/test?ssl=true&replicaSet=580scheduleDB-shard-0&authSource=admin";
+		MongoClientURI clientUri = new MongoClientURI(uri);
+		MongoClient mongoClient = new MongoClient(clientUri);
+		MongoDatabase mongoDatabase = mongoClient.getDatabase("580Schedule");
+		MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("Users");
 		
 //		used to test the connection
 //		Document document = new Document("EID", "11");
@@ -38,16 +39,54 @@ public class App {
 //		
 //		mongoCollection.insertOne(document);
 		
+		//Used to update only one document by a filter ("Name") and field("Availability")
+//		Bson filter = new Document("Name", "Roger");
+//		Bson newValue = new Document("Availability", "Unavailable");
+//		Bson updateOperationDocument = new Document("$set", newValue);
+//		mongoCollection.updateOne(filter, updateOperationDocument);
+		
+		//updating a sub document
+//		Bson filter = new Document("Name", "Roger");
+//		Bson newValue = new Document("TimeAvailable.Monday.Start1", "9");
+//		Bson updateOperationDocument = new Document("$set", newValue);
+//		mongoCollection.updateOne(filter, updateOperationDocument);
+		
+		//updated everyone with the new documents
+//		Document document = new Document("Start1", "");
+//		document.append("End1", "");
+//		document.append("Start2", "");
+//		document.append("End2", "");
+//		document.append("Start3", "");
+//		document.append("End3", "");
+//		
+//		BasicDBObject Available = new BasicDBObject();
+//		Available.put("Monday",  document);
+//		Available.put("Tuesday",document );
+//		Available.put("Wednesday",document );
+//		Available.put("Thursday", document);
+//		Available.put("Friday", document);
+//		Available.put("Saturday", document);
+//		Available.put("Sunday", document);
+//		Bson update = new Document("TimeAvailable", Available);
+//		Bson updateOperationDocument = new Document("$set", update);
+//		mongoCollection.updateMany(Filters.exists("Name"), updateOperationDocument);
+		
 		//used to read from the database by a specific Filter
 		//filter can be "EID", "Name", "Availability", "Username", "Password"
-//		try{
-//			Document myDoc = mongoCollection.find(Filters.eq("Name", "Roger")).first();
-//			System.out.println(myDoc.get("Availability"));
-//		}
-//		catch(Exception e) 
-//		{
-//			System.out.println("Failed to find doc");
-//		}
+		
+		//how read sub documents
+//		Document subDoc = (Document) myDoc.get("TimeAvailable");
+//		Document subSubDoc = (Document) subDoc.get("Monday");
+//		System.out.println(subSubDoc.get("Start1"));
+		
+		try{
+			Document myDoc = mongoCollection.find(Filters.eq("Name", "Roger")).first();
+			System.out.println(myDoc.get("Name"));
+		}
+		catch(Exception e) 
+		{
+			System.out.println("Failed to find doc");
+		}
 		
 		
 		
